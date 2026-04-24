e5225daebb307ccd2d6ef815982af0ad0f93058b
Hasan <hasan.myoffice2@gmail.com>
2026-04-24 20:55:31 +0600

    Projects Complete

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,335 @@
+<script setup>
+</script>
+
+<template>
+  <!-- Navabr Section -->
+  <nav>
+    <div class="container">
+      <div class="nav-container">
+        <div class="nav-menu">
+          <div class="brand-name">
+            <img src="/public/image/brand.png" alt="">
+          </div>
+          <ul class="nav-menu">
+            <li><a class="active" href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="input-aria">
+          <input type="text" name="" id="">
+          <div class="avator">
+            <img src="/public/image/download (1).jpg" alt="">
+          </div>
+        </div>
+        <!-- mobile-menu -->
+        <div class="mobile-menu"><i class="fa-solid fa-bars"></i></div>
+      </div>
+    </div>
+  </nav>
+  <!-- Navabr End -->
+
+  <!-- Hero-Section -->
+  <section class="hero-section">
+    <div class="container">
+      <div class="hero-aria">
+        <div class="image">
+          <img src="/public/image/hero.jpg" alt="">
+        </div>
+        <div class="hero-title">
+          <div class="agency">
+            <span>Agency</span>
+            <p>13th March 2023</p>
+          </div>
+          <h1>Let's Collaborate: We Provide Management Services</h1>
+          <p>This is required when, for example, the final text is not yet available. <br> Dummy text is also known as
+            'fill
+            text'.</p>
+          <a href="#">Reed More</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Hero-End -->
+
+  <!-- Features Section -->
+  <section class="features-section">
+    <div class="container">
+      <h3>Features</h3>
+      <div class="features-card">
+        <div class="card-items">
+          <div class="feature-icons">
+            <i class="bi bi-grid"></i>
+          </div>
+          <h4>Trending</h4>
+        </div>
+        <div class="card-items">
+          <div class="feature-icons">
+            <i class="bi bi-grid"></i>
+          </div>
+          <h4>Business</h4>
+        </div>
+        <div class="card-items">
+          <div class="feature-icons">
+            <i class="bi bi-grid"></i>
+          </div>
+          <h4>eCommerce</h4>
+        </div>
+        <div class="card-items">
+          <div class="feature-icons">
+            <i class="bi bi-grid"></i>
+          </div>
+          <h4>Business</h4>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Features End -->
+
+  <!-- Trending Scetion -->
+  <section class="blog-section">
+    <div class="blog-container">
+      <div class="trending-title">
+        <h2>Trending Title</h2>
+        <button>See More</button>
+      </div>
+      <div class="blog-aria">
+        <div class="blog-card">
+          <div class="card-items">
+            <img src="/public/image/trending.jpg" alt="blog-card">
+            <div class="card-content">
+              <ul class="card-title">
+                <span>eCommerce</span>
+                <li>29th Nov 2023</li>
+              </ul>
+              <h3>on the other hand we provite denounce</h3>
+              <a href="#">Reed More</a>
+            </div>
+          </div>
+          <div class="card-items">
+            <img src="/public/image/trending.jpg" alt="blog-card">
+            <div class="card-content">
+              <ul class="card-title">
+                <span>eCommerce</span>
+                <li>29th Nov 2023</li>
+              </ul>
+              <h3>on the other hand we provite denounce</h3>
+              <a href="#">Reed More</a>
+            </div>
+          </div>
+          <div class="card-items">
+            <img src="/public/image/trending.jpg" alt="blog-card">
+            <div class="card-content">
+              <ul class="card-title">
+                <span>eCommerce</span>
+                <li>29th Nov 2023</li>
+              </ul>
+              <h3>on the other hand we provite denounce</h3>
+              <a href="#">Reed More</a>
+            </div>
+          </div>
+          <div class="card-items">
+            <img src="/public/image/trending.jpg" alt="blog-card">
+            <div class="card-content">
+              <div class="card-title">
+                <span>eCommerce</span>
+                <p>29th Nov 2023</p>
+              </div>
+              <h3>on the other hand we provite denounce</h3>
+              <a href="#">Reed More</a>
+            </div>
+          </div>
+        </div>
+        <div class="Search-Section">
+          <div class="search-aria">
+            <h5>Search</h5>
+            <input type="text" placeholder="SearchKeyword">
+            <h5 class="recent-post">Recent Post</h5>
+            <div class="recent-aria">
+              <img src="/public/image/1.66c0101b3d4b0cb98955.jpg" alt="image">
+              <div class="recent-title">
+                <h6>Consultant Business</h6>
+                <p>13th March 2023</p>
+              </div>
+            </div>
+            <div class="recent-aria">
+              <img src="/public/image/newslater.jpg" alt="image">
+              <div class="recent-title">
+                <h6>Consultant Business</h6>
+                <p>13th March 2023</p>
+              </div>
+            </div>
+            <div class="recent-aria">
+              <img src="/public/image/trending.jpg" alt="image">
+              <div class="recent-title">
+                <h6>Consultant Business</h6>
+                <p>13th March 2023</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Trending End -->
+
+  <!-- NewsLeter Section  -->
+  <section class="newslater-section">
+    <div class="container">
+      <div class="newslaeter">
+        <div class="news-title">
+          <h3>Enough Talk, Let's Build Something Together</h3>
+          <p>Sync designers, developers, managers, contractors, and stakeholders in one cloud-based tool.</p>
+        </div>
+        <button>Boots Your Site</button>
+      </div>
+    </div>
+  </section>
+  <!-- NewsLeter End  -->
+
+  <!-- Busness Section -->
+  <section class="busnes-section">
+    <div class="busness-cntainer">
+      <div class="busness-title">
+        <h2>Busness</h2>
+        <button>See More</button>
+      </div>
+      <div class="busness-card">
+        <div class="busnes-card-items">
+          <img src="/public/image/trending.jpg" alt="">
+          <div class="busness-content">
+            <ul class="card-baged">
+              <span>eCommerce</span>
+              <li>29th Nov 2023</li>
+            </ul>
+            <h3>On the other hand we provide denounce</h3>
+            <a href="#">Reed More</a>
+          </div>
+        </div>
+        <div class="busnes-card-items">
+          <img src="/public/image/trending.jpg" alt="">
+          <div class="busness-content">
+            <ul class="card-baged">
+              <li>eCommerce</li>
+              <li>29th Nov 2023</li>
+            </ul>
+            <h3>On the other hand we provide denounce</h3>
+            <a href="#">Reed More</a>
+          </div>
+        </div>
+        <div class="busnes-card-items">
+          <img src="/public/image/trending.jpg" alt="">
+          <div class="busness-content">
+            <ul class="card-baged">
+              <li>eCommerce</li>
+              <li>29th Nov 2023</li>
+            </ul>
+            <h3>On the other hand we provide denounce</h3>
+            <a href="#">Reed More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Busness End -->
+  <!-- Busness Section -->
+  <section class="busnes-section">
+    <div class="busness-cntainer">
+      <div class="busness-title">
+        <h2>eCommerce</h2>
+        <button>See More</button>
+      </div>
+      <div class="busness-card">
+        <div class="busnes-card-items">
+          <img src="/public/image/trending.jpg" alt="">
+          <div class="busness-content">
+            <ul class="card-baged">
+              <li>eCommerce</li>
+              <li>29th Nov 2023</li>
+            </ul>
+            <h3>On the other hand we provide denounce</h3>
+            <a href="#">Reed More</a>
+          </div>
+        </div>
+        <div class="busnes-card-items">
+          <img src="/public/image/trending.jpg" alt="">
+          <div class="busness-content">
+            <ul class="card-baged">
+              <li>eCommerce</li>
+              <li>29th Nov 2023</li>
+            </ul>
+            <h3>On the other hand we provide denounce</h3>
+            <a href="#">Reed More</a>
+          </div>
+        </div>
+        <div class="busnes-card-items">
+          <img src="/public/image/trending.jpg" alt="">
+          <div class="busness-content">
+            <ul class="card-baged">
+              <li>eCommerce</li>
+              <li>29th Nov 2023</li>
+            </ul>
+            <h3>On the other hand we provide denounce</h3>
+            <a href="#">Reed More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Busness End -->
+
+  <!-- Footer Section -->
+  <section class="footer-section">
+    <div class="footer-container">
+      <div class="footer-card">
+        <div class="footer-items">
+          <img src="/public/image/brand.png" alt="image">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ex quae id illo eos eligendi.</p>
+          <ul class="footer-icons">
+            <li><i class="bi bi-facebook"></i></li>
+            <li><i class="bi bi-instagram"></i></li>
+            <li><i class="bi bi-twitter"></i></li>
+            <li><i class="bi bi-github"></i></li>
+            <li><i class="bi bi-linkedin"></i></li>
+            <li><i class="bi bi-whatsapp"></i></li>
+          </ul>
+        </div>
+        <div class="footer-items">
+          <h3>Company</h3>
+          <ul class="footer-link">
+            <li><i class="bi bi-chevron-right"></i> About us</li>
+            <li><i class="bi bi-chevron-right"></i> Services</li>
+            <li><i class="bi bi-chevron-right"></i> Team</li>
+            <li><i class="bi bi-chevron-right"></i> Pricing</li>
+            <li><i class="bi bi-chevron-right"></i> Project</li>
+          </ul>
+        </div>
+        <div class="footer-items">
+          <h3>Usefull Links</h3>
+          <ul class="footer-link">
+            <li><i class="bi bi-chevron-right"></i> Terms of Services</li>
+            <li><i class="bi bi-chevron-right"></i> Privacy Policy</li>
+            <li><i class="bi bi-chevron-right"></i> Documentation</li>
+            <li><i class="bi bi-chevron-right"></i> Changelog</li>
+            <li><i class="bi bi-chevron-right"></i> Components</li>
+          </ul>
+        </div>
+        <div class="footer-items">
+          <h3>Question?</h3>
+          <ul class="footer-link">
+            <li><i class="bi bi-envelope"></i> Drop us a line at:</li>
+            <li>contact@example.com</li>
+            <li><i class="bi bi-telephone"></i> Call now:</li>
+            <li>+152534-468-854</li>
+          </ul>
+        </div>
+      </div>
+      <hr class="divider">
+      <ul class="copy-right">
+        <li>© 2026 Getblog. Design & Develop with ❤️ by Shreethemes.</li>
+        <li>Privacy &nbsp; Terms &nbsp; FAQ &nbsp; Contact</li>
+      </ul>
+    </div>
+  </section>
+  <!-- Footer End -->
+</template>
